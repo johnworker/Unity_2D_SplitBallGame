994872925170fb04ce86fefc160ab1080a1d88fa
hunte <besscomdark@gmail.com>
2024-07-21 23:15:52 +0800

    調整

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,57 +1,39 @@
+ï»¿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public GameObject ballPrefab;
-    public int scoreIncrement = 10;
-    private bool isSplit = false;
-    private GameManager gameManager;
+    public GameObject ballPrefab; // åˆ†è£‚çƒçš„é è£½ä»¶
+    public int initialBallCount = 30; // åˆå§‹åˆ†è£‚çƒæ•¸é‡
+    public float ballSpeed = 5f; // åˆ†è£‚çƒç§»å‹•é€Ÿåº¦
+    public Transform spawnPoint; // åˆ†è£‚çƒç”Ÿæˆé»
+
+    private List<GameObject> balls = new List<GameObject>();
 
     void Start()
     {
-        gameManager = FindObjectOfType<GameManager>();
+        for (int i = 0; i < initialBallCount; i++)
+        {
+            SpawnBall();
+        }
+    }
+
+    public void SpawnBall()
+    {
+        GameObject newBall = Instantiate(ballPrefab, spawnPoint.position, Quaternion.identity);
+        newBall.GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle * ballSpeed;
+        balls.Add(newBall);
     }
 
     void Update()
     {
-        Move();
-        if (Input.GetKeyDown(KeyCode.Space) && !isSplit)
+        foreach (GameObject ball in balls)
         {
-            Split();
-        }
-    }
-
-    void Move()
-    {
-        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        transform.Translate(new Vector3(moveX, moveY, 0));
-    }
-
-    void Split()
-    {
-        isSplit = true;
-        GameObject newBall1 = Instantiate(ballPrefab, transform.position + Vector3.right, Quaternion.identity);
-        GameObject newBall2 = Instantiate(ballPrefab, transform.position + Vector3.left, Quaternion.identity);
-
-        newBall1.GetComponent<BallController>().isSplit = true;
-        newBall2.GetComponent<BallController>().isSplit = true;
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            Destroy(gameObject); // ¸I¨ì»ÙÃªª«®É¾P·´²yÅé
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("ScoreZone"))
-        {
-            gameManager.IncreaseScore(scoreIncrement); // ¶i¤J±o¤À°Ï°ì®É¼W¥[¤À¼Æ
+            if (ball.transform.position.magnitude > 10f)
+            {
+                ball.GetComponent<Rigidbody2D>().velocity = -ball.GetComponent<Rigidbody2D>().velocity;
+            }
         }
     }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public BallController ballController;
+
+    void Start()
+    {
+        ballController = FindObjectOfType<BallController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ballController.SpawnBall();
+        }
+    }
+}
