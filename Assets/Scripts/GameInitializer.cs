7595229da5ef5fd64ecabfc28c704ac8b1e60867
hunte <besscomdark@gmail.com>
2024-07-22 22:39:53 +0800

    調整

---
+++ Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameInitializer : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public Transform[] spawnPoints;
+
+    void Start()
+    {
+        foreach (var spawnPoint in spawnPoints)
+        {
+            Instantiate(ballPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
